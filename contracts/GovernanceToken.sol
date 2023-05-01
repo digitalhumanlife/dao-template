480ed4c75ebb20f1801f7d1cfc86765353584e52
JW <digitalhumanlife@gmail.com>
2023-05-01 21:57:34 +0900

    first commit

---
+++ contracts/GovernanceToken.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract GovernanceToken is ERC20Votes {
+    
+    uint256 public s_maxSupply = 1000000000000000000000000;
+
+    constructor() ERC20("GovernanceToken", "GT") ERC20Permit("GovernanceToken") {
+        _mint(msg.sender, s_maxSupply);
+    }
+        
+    }
+}
